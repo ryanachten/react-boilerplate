33a124aee73c0223ee1606f348e23226ec83e18b
Ryan Achten <ryanachten@gmail.com>
2018-01-10 10:33:30 +1300

    Add boilerplate version with client-side routing setup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import AppName from './components/AppName';
+import AppRouter from './routers/AppRouters.js'
 
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render( <p>AppName component goes here</p>, document.getElementById('app') );
+
+ReactDOM.render( <AppRouter />, document.getElementById('app') );
